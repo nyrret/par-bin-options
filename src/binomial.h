35ac9a6b05f90a0cdd2ff2a2d485657867ef9f70
Terryn Brunelle <terryn@studioterryn.com>
2022-01-07 12:46:14 -0500

    american call option binomial

--- src/binomial.h
+++ src/binomial.h
@@ -1,3 +1,4 @@
 #include <cstdint>
 
 double americanPut(uint16_t T, double S, double K, double r, double sigma, double q, uint16_t n);
+double americanCall(uint16_t T, double S, double K, double r, double sigma, double q, uint16_t n);
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 
 #include "src/main.h"
-#include "src/binomial.h"
+#include "binomial.h"
 
 int main(int argc, char* argv[]) {
-  double result = americanPut(4, 4, 4, 4, 4, 4, 4);
+  double result = americanCall(4, 4, 4, 4, 4, 4, 4);
   std::cout << "result: " << result << std::endl;
 }
