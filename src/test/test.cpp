230200a4d6384259cc407bdc97e610ad6aadb556
Terryn Brunelle <terryn@studioterryn.com>
2022-02-04 00:29:05 -0500

    zubair binomial tests

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -56,6 +56,61 @@ TEST(BinomialTest, americanPutQLTest)
   EXPECT_LT(result, 10.48815);
 }
 
+// Zubair tests
+TEST(BinomialTest, europeanCallZubairTest)
+{
+  double result = Binomial::binomialTraversal<Binomial::ZubairEuropeanCall>(100, 252, 127.62, 130, 0.001, 0.20, 0.0163);
+
+  // should be approx 6.7695
+  EXPECT_GE(result, 6.76945);
+  EXPECT_LT(result, 6.76955);
+}
+
+TEST(BinomialTest, europeanCallZubairNoDividendTest)
+{
+  double result = Binomial::binomialTraversal<Binomial::ZubairEuropeanCall>(100, 252, 127.62, 130, 0.001, 0.20);
+
+  // should be approx 7.4510
+  EXPECT_GE(result, 7.4505);
+  EXPECT_LT(result, 7.4515);
+}
+
+TEST(BinomialTest, europeanPutZubairTest)
+{
+  double result = Binomial::binomialTraversal<Binomial::ZubairEuropeanPut>(100, 252, 127.62, 130, 0.001, 0.20, 0.0163);
+
+  // should be approx 10.4880
+  EXPECT_GE(result, 10.48795);
+  EXPECT_LT(result, 10.4881);
+}
+
+TEST(BinomialTest, americanCallZubairTest)
+{
+  double result = Binomial::binomialTraversal<Binomial::ZubairAmericanCall>(100, 252, 127.62, 130, 0.001, 0.20, 0.0163);
+
+  // should be approx 6.8586
+  EXPECT_GE(result, 6.85855);
+  EXPECT_LT(result, 6.85865);
+}
+
+TEST(BinomialTest, americanCallZubairNoDividendTest)
+{
+  double result = Binomial::binomialTraversal<Binomial::ZubairAmericanCall>(100, 252, 127.62, 130, 0.001, 0.20);
+
+  // should be approx 7.4510
+  EXPECT_GE(result, 7.4505);
+  EXPECT_LT(result, 7.4515);
+}
+
+TEST(BinomialTest, americanPutZubairTest)
+{
+  double result = Binomial::binomialTraversal<Binomial::ZubairAmericanPut>(100, 252, 127.62, 130, 0.001, 0.20, 0.0163);
+
+  // should be approx 10.4881
+  EXPECT_GE(result, 10.48795);
+  EXPECT_LT(result, 10.48815);
+}
+
 int main(int argc, char* argv[])
 {
   ::testing::InitGoogleTest(&argc, argv);
